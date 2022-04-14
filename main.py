3ff4034affc9edb305f75bedb034a01712fef588
KimgzzzR <103601456+KimgzzzR@users.noreply.github.com>
2022-04-14 08:23:20 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 Music Player, Telegram Voice Chat Bot
-Copyright (c) 2021-present Asm Safone <https://github.com/AsmSafone>
+
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
@@ -36,7 +36,7 @@ from core import (
 
 REPO = """
 🤖 **Music Player**
-- Repo: [GitHub](https://github.com/AsmSafone/MusicPlayer)
+
 - License: AGPL-3.0-or-later
 """
 
